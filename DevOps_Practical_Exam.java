e3aad1e2d79646af747de96824a3e92cec1de14b
surveadi2000 <154872128+surveadi2000@users.noreply.github.com>
2024-04-08 12:34:08 +0530

    Add files via upload

---
+++ DevOps_Practical_Exam.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devops_practical_exam;
+
+/**
+ *
+ * @author user
+ */
+public class DevOps_Practical_Exam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 5;
+        int num2 = 10;
+        int multiply = num1 * num2;
+        System.out.println("Multiplication result : " + multiply);
+    }
+    
+}
